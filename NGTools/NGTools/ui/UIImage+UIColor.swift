ce88c6208105674158a3c5291c03999cc9c98079
Deloge, Xavier <XDeloge@lapresse.ca>
2018-11-12 08:56:51 -0500

    Removed convenience constructor using color to create an image (didn't work)

--- NGTools/NGTools/ui/UIImage+UIColor.swift
+++ NGTools/NGTools/ui/UIImage+UIColor.swift
@@ -9,17 +9,6 @@
 import UIKit
 
 public extension UIImage {
-    public convenience init?(color: UIColor, size: CGSize) {
-        let newImage = color.image(size: size)
-        if let ciImage = newImage?.ciImage {
-            self.init(ciImage: ciImage)
-        }
-        return nil
-    }
-
-    @objc public convenience init?(color: UIColor, andWidth width: Int) {
-        self.init(color: color, size: CGSize(width: width, height: 1))
-    }
 
     @objc public static func image(color: UIColor, size: CGSize) -> UIImage? {
         return color.image(size: size)
