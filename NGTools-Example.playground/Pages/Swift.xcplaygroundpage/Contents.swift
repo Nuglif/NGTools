dde42f14fb0a1932b09f4f7f8c06610dd79d9bf2
Ayrton <awerck@nuglif.com>
2019-04-08 15:34:48 -0400

    Updated playground and removed compactMapValues as it is provided by swift 5 now

--- NGTools-Example.playground/Pages/Swift.xcplaygroundpage/Contents.swift
+++ NGTools-Example.playground/Pages/Swift.xcplaygroundpage/Contents.swift
@@ -1,18 +1,5 @@
 //: [Previous](@previous)
 
 import Foundation
-import NGTools
-
-let greenVegetables = ["🍏": 1, "🥒": 7, "🥦": 5, "🥑": 6]
-let redVegetables: [String: Any] = ["🌶": 1, "🍓": 4, "🍎": "notANumber"]
-
-let castInt: (Any) throws -> Int? = { value in
-    return value as? Int
-}
-
-let mergedVegetables = try? greenVegetables
-    .merging(redVegetables.compactMapValues(castInt), uniquingKeysWith: ( + ))
-
-print(mergedVegetables)
 
 //: [Next](@next)
