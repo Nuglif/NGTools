a346f0c68cc1c78791059a4802a464256d943d16
Jeremy Goldschmidt <jgoldschmidt@nuglif.com>
2019-05-13 16:44:03 -0400

    correction code review

--- NGTools/NGTools/String.swift
+++ NGTools/NGTools/String.swift
@@ -14,21 +14,16 @@ import func CommonCrypto.CC_SHA1
 import typealias CommonCrypto.CC_LONG
 
 public extension String {
-    var md5: String? {
-        let length = Int(CC_MD5_DIGEST_LENGTH)
-        let messageData = self.data(using:.utf8)!
-        var digestData = Data(count: length)
 
-        _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
-            messageData.withUnsafeBytes { messageBytes -> UInt8 in
-                if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
-                    let messageLength = CC_LONG(messageData.count)
-                    CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
-                }
-                return 0
-            }
+    var md5: String {
+        let data = Data(self.utf8)
+
+        var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+        _ = data.withUnsafeBytes {
+            CC_MD5($0.baseAddress, UInt32(data.count), &digest)
         }
-        return String(data: digestData, encoding: .utf8)
+
+        return digest.map { String(format: "%02x", $0) }.joined()
     }
 
     var sha1: String {
@@ -53,12 +48,12 @@ public extension String {
 }
 
 public extension NSString {
-    @objc func md5() -> NSString? {
-        return (self as String).md5 as NSString?
+    @objc func md5() -> NSString {
+        return (self as String).md5 as NSString
     }
 
-    @objc func sha1() -> NSString? {
-        return (self as String).sha1 as NSString?
+    @objc func sha1() -> NSString {
+        return (self as String).sha1 as NSString
     }
 
     @objc func isEmptyOrContainsOnlyWhitespaces() -> Bool {
