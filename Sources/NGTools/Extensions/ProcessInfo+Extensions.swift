3e5975c7d67739537d6a85b14f425190585933ef
Olivier Fournier <ofournier@nuglif.com>
2023-06-13 11:44:31 -0400

    New extension

---
+++ Sources/NGTools/Extensions/ProcessInfo+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  ProcessInfo+Extensions.swift
+//  NGTools
+//
+//  Created by Olivier Fournier on 2023-06-13.
+//  Copyright © 2023 Nuglif. All rights reserved.
+//
+
+import Foundation
+
+public extension ProcessInfo {
+
+    var isAppRunningOnMac: Bool {
+        guard #available(iOS 14.0, *) else { return false }
+
+        return isiOSAppOnMac
+    }
+}
