f430cfd6713b26f217200a7dc6ccddeeb9009a1b
N'rick <njeannecreantor@nuglif.com>
2024-06-05 11:01:37 -0400

    [TEST] Thread safe contain and remove task

--- Tests/NGToolsTests/ThreadSafeSetTests.swift
+++ Tests/NGToolsTests/ThreadSafeSetTests.swift
@@ -15,14 +15,41 @@ final class ThreadSafeSetTests: XCTestCase {
         let set = ThreadSafeSet<UUID>()
         let expectation = XCTestExpectation(description: "Testing")
 
-        let queueA = DispatchQueue.init(label: "TestQueueA", qos: .background)
+        let queueA = DispatchQueue(label: "TestQueueA", qos: .background)
         let queueB = DispatchQueue(label: "TestQueueB", qos: .background)
         let iterations = 1000
-        let requests = Array.init(repeating: UUID(), count: iterations)
+        let requests = Array(repeating: UUID(), count: iterations)
         var completed = 0
 
         requests.forEach { request in
             queueA.async { set.insert(request) }
+
+            queueB.async {
+                set.remove(request)
+                completed += 1
+                if completed == iterations - 1 {
+                    expectation.fulfill()
+                }
+            }
+        }
+
+        wait(for: [expectation], timeout: 3)
+    }
+
+    func testConcurrencyContainsAndRemove() {
+        let expectation = XCTestExpectation(description: "Testing")
+
+        let queueA = DispatchQueue(label: "TestQueueA", qos: .background)
+        let queueB = DispatchQueue(label: "TestQueueB", qos: .background)
+        let iterations = 1000
+        let requests = Array(repeating: UUID(), count: iterations)
+        let set = ThreadSafeSet<UUID>(set: Set(requests))
+        var completed = 0
+
+        requests.forEach { request in
+            queueA.async {
+                _ = set.contains(request)
+            }
 
             queueB.async {
                 set.remove(request)
