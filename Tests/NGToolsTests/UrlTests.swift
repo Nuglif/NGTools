9cafa14fdddade371ccab624ff09bf0894f14b04
Judith C Untiveros <judifis2706@gmail.com>
2025-01-08 10:07:26 -0500

    Rubi 8549540 supporter le format preroll (#27)
    * adding character | in urlEncoded
    
    * adding tests
    
    ---------
    
    Co-authored-by: jcondor <jcondor@nuglif.com>

--- Tests/NGToolsTests/UrlTests.swift
+++ Tests/NGToolsTests/UrlTests.swift
@@ -14,8 +14,8 @@ import XCTest
 final class UrlTests: XCTestCase {
 
     func testUrlEncoding() {
-        let value1 = "!#$%&'()*+,/:;=?@[ ]".encoded
-        let expectedValue1 = "%21%23%24%25%26%27%28%29%2A%2B%2C%2F%3A%3B%3D%3F%40%5B%20%5D"
+        let value1 = "|!#$%&'()*+,/:;=?@[ ]".encoded
+        let expectedValue1 = "%7C%21%23%24%25%26%27%28%29%2A%2B%2C%2F%3A%3B%3D%3F%40%5B%20%5D"
 
         XCTAssertTrue(value1 == expectedValue1)
 
@@ -67,6 +67,22 @@ final class UrlTests: XCTestCase {
         let expectedUrl =
             """
             https://pubads.g.doubleclick.net/gampad/ads?ad_rule=1&cust_params=articleId%3D4a0c93a9ef8a3efdab0b7a345ecc59fe%26key1%3Dvalue1%26key2%3Dvalue2%26microtheme%3Dcoronavirus%26publicationDate%3D2020-12-06%26sectionName%3Dfootball%26suptitle%3Dsports-illustrated%26typePage%3Darticle%26videoDuration%3Dvideo31-60&env=vp&gdfp_req=1&hl=fr&iu=%2F21686484267%2FLPM%2FLPM_Sports&output=vast&sz=640x480&unviewed_position_start=1
+            """
+        let parameters: [String: Any] = ["key1": "value1", "key2": "value2"]
+        let components = baseUrl.integrate(parameters)
+
+        XCTAssertTrue(components?.url?.absoluteString == expectedUrl)
+    }
+    
+    // swiftlint:disable line_length
+    func testUrlComponentsWithAdTagUrl2() {
+        let baseUrl =
+            """
+            https://pubads.g.doubleclick.net/gampad/ads?gdfp_req=1&env=vp&unviewed_position_start=1&sz=480x640%7C640x480&iu=%2F21686484267%2FLPM%2FLPM_Sports&ad_rule=1&cust_params=typePage%3Darticle%26articleId%3D4a0c93a9ef8a3efdab0b7a345ecc59fe%26suptitle%3Dsports-illustrated%26publicationDate%3D2020-12-06%26microtheme%3Dcoronavirus%26sectionName%3Dfootball%26videoDuration%3Dvideo31-60&hl=fr&output=vast
+            """
+        let expectedUrl =
+            """
+            https://pubads.g.doubleclick.net/gampad/ads?ad_rule=1&cust_params=articleId%3D4a0c93a9ef8a3efdab0b7a345ecc59fe%26key1%3Dvalue1%26key2%3Dvalue2%26microtheme%3Dcoronavirus%26publicationDate%3D2020-12-06%26sectionName%3Dfootball%26suptitle%3Dsports-illustrated%26typePage%3Darticle%26videoDuration%3Dvideo31-60&env=vp&gdfp_req=1&hl=fr&iu=%2F21686484267%2FLPM%2FLPM_Sports&output=vast&sz=480x640%7C640x480&unviewed_position_start=1
             """
         let parameters: [String: Any] = ["key1": "value1", "key2": "value2"]
         let components = baseUrl.integrate(parameters)
