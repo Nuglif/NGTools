b006e632a441bfb7cb87891a2f3dc908042172a8
Romain Lambert <rlambert@nuglif.com>
2025-01-10 11:53:42 -0500

    Replace UIGraphicsBeginImageContext with UIGraphicsImageRenderer

--- Sources/NGTools/Extensions/UIImage+UIColor.swift
+++ Sources/NGTools/Extensions/UIImage+UIColor.swift
@@ -19,15 +19,15 @@ public extension UIImage {
     }
 
     convenience init?(color: UIColor) {
-        let rect = CGRect(origin: CGPoint.zero, size: CGSize(width: 1, height: 1))
-        UIGraphicsBeginImageContext(rect.size)
+        let rect: CGRect = .init(x: 0, y: 0, width: 1, height: 1)
+        let format = UIGraphicsImageRendererFormat.default()
+        format.opaque = false
 
-        guard let context = UIGraphicsGetCurrentContext() else { return nil }
-        context.setFillColor(color.cgColor)
-        context.fill(rect)
-
-        guard let image = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
-        UIGraphicsEndImageContext()
+        let renderer = UIGraphicsImageRenderer(size: rect.size, format: format)
+        let image = renderer.image { context in
+            context.cgContext.setFillColor(color.cgColor)
+            context.cgContext.fill(rect)
+        }
 
         guard let cgImage = image.cgImage else { return nil }
         self.init(cgImage: cgImage)
@@ -43,25 +43,24 @@ public extension UIImage {
         let originalResizingMode = resizingMode
         let originalAlignmentRectInsets = alignmentRectInsets
 
-        UIGraphicsBeginImageContextWithOptions(size, false, scale)
-        guard let context = UIGraphicsGetCurrentContext() else { return self }
+        let format = UIGraphicsImageRendererFormat.default()
+        format.opaque = false
 
-        // Flip the context vertically
-        context.translateBy(x: 0.0, y: size.height)
-        context.scaleBy(x: 1.0, y: -1.0)
+        let renderer = UIGraphicsImageRenderer(size: size, format: format)
+        var image = renderer.image { context in
+            // Flip the context vertically
+            context.cgContext.translateBy(x: 0.0, y: size.height)
+            context.cgContext.scaleBy(x: 1.0, y: -1.0)
 
-        // Image tinting mostly inspired by http://stackoverflow.com/a/22528426/255489
-        context.setBlendMode(.normal)
-        context.draw(cgImage!, in: imageRect)
+            // Image tinting mostly inspired by http://stackoverflow.com/a/22528426/255489
+            context.cgContext.setBlendMode(.normal)
+            context.cgContext.draw(cgImage!, in: imageRect)
 
-        // .sourceIn: resulting color = source color * destination alpha
-        context.setBlendMode(.sourceIn)
-        context.setFillColor(color.cgColor)
-        context.fill(imageRect)
-
-        // Get new image
-        guard var image = UIGraphicsGetImageFromCurrentImageContext() else { return self }
-        UIGraphicsEndImageContext()
+            // .sourceIn: resulting color = source color * destination alpha
+            context.cgContext.setBlendMode(.sourceIn)
+            context.cgContext.setFillColor(color.cgColor)
+            context.cgContext.fill(imageRect)
+        }
 
         // Prevent further tinting
         image = image.withRenderingMode(.alwaysOriginal)
